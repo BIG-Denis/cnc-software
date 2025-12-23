8bd3a5625f0b8bf2f00417ea9ba56d39c9e32b91
BIG-Denis <lozejor12@gmail.com>
2025-12-23 23:43:05 +0300

    [advanced_ui] Massive update

--- gui/main.py
+++ gui/main.py
@@ -4,8 +4,10 @@ from PyQt5 import uic
 from PyQt5.QtCore import Qt, QObject, QEvent
 from PyQt5.QtGui import QTextCursor, QPixmap
 from PIL import Image, ImageDraw
+import os
 import sys
 import copy
+import math
 #import serial.tools.list_ports as get_list
 
 # from response import *
@@ -13,6 +15,17 @@ import copy
 def SendGcode(port: str, gcodeLines: list[str], baudrate: int, chunkSize: int=250, retries: int=3) -> bool:
     print(f"Sending G-codes: port={port}, baudrate={baudrate}; G-codes={gcodeLines}")
     return True
+
+
+def nearest_anchor(x: int, y: int, anchors: set[tuple[int, int]]) -> tuple[int, int]:
+    if not anchors:
+        raise ValueError("Множество anchors не должно быть пустым")
+    return min(anchors, key=lambda a: math.hypot(a[0] - x, a[1] - y))
+
+
+def get_gcode(mode: str, paint: bool, x: int, y: int) -> str:
+    if mode in ('hrz', 'vrt', 'slp'):
+        return f'G{"01" if paint else "00"} X{x} Y{y}'
 
 
 def confirm(parent=None, text="Точно?") -> bool:
@@ -42,26 +55,31 @@ class MainWindow(QMainWindow):
         self.ui = uic.loadUi('mainwindow.ui', self)
         self.mainTabs.setCurrentIndex(0)
         self.GetCOMPorts()
-        self.filter = GraphicsViewClickFilter(self.on_field_view_clicked)
-        self.field_view.viewport().installEventFilter(self.filter)
+        self.filter = GraphicsViewClickFilter(self.on_paint_view_clicked)
+        self.paint_view.viewport().installEventFilter(self.filter)
         self.scene = QGraphicsScene()
-        self.field_view.setScene(self.scene)
-        self.field_view.fitInView(self.scene.itemsBoundingRect(), Qt.KeepAspectRatio)
-        self.mode_label.setText(f'Ожидание выбора команды')
+        self.paint_view.setScene(self.scene)
+        self.paint_view.fitInView(self.scene.itemsBoundingRect(), Qt.KeepAspectRatio)
+        self.mode_paint_label.setText(f'Горизонтальное перемещение')
 
-        self.btn_refresh.clicked.connect(self.GetCOMPorts)
-        self.btn_colebrate.clicked.connect(self.clicked_btn_calibrate)
-        self.btn_clear_img.clicked.connect(self.clicked_btn_clear_img)
-        self.btn_g00.clicked.connect(self.clicked_btn_g00)
-        self.btn_g01.clicked.connect(self.clicked_btn_g01)
+        # !TODO: button clock calls
+        self.btn_dump_codes.clicked.connect(self.clicked_btn_dump_codes)
+        self.btn_paint_calibrate.clicked.connect(self.clicked_btn_calibrate)
+        self.btn_paint_clear_img.clicked.connect(self.clicked_btn_clear_img)
+        self.btn_paint_vertical.clicked.connect(self.clicked_btn_mode_vrt)
+        self.btn_paint_horizontal.clicked.connect(self.clicked_btn_mode_hrz)
+        self.btn_paint_sloped.clicked.connect(self.clicked_btn_mode_slp)
         # TBD
 
-        self.mode: str = 'G00'  # G00, G01, G02, G03, G90, G91
+        self.mode: str = 'hrz'  # hrz, vrt, slp, htc, arc
+        self.drawing: bool = self.btn_radio_paint.isChecked()
         self.current_x: int = 0
         self.current_y: int = 0
         self.goto_x: int = None
         self.goto_y: int = None
-        self.draw_commands: list = []  # G00/G01, x1, y1, x2, y2
+        self.draw_commands: list = []
+        self.g_codes: list = []
+        self.anchors: set = {(0, 0),}
 
         self.draw_img()
 
@@ -73,9 +91,10 @@ class MainWindow(QMainWindow):
             local_commands.append(pre)
         # all commands
         for command in local_commands:
-            mode, x1, y1, x2, y2 = command
-            if mode == 'G01':
+            if command[0] in ('hrz', 'vrt', 'slp'):
+                x1, y1, x2, y2 = command[1:]
                 draw.line((x1, y1, x2, y2), fill="black" if command != pre else 'green', width=2)
+            pass
         # current position
         if self.current_x is not None and self.current_y is not None:
             r = 3  # радиус текущей позиции
@@ -104,56 +123,78 @@ class MainWindow(QMainWindow):
         cursor.insertText(text)
         self.consoleEdit.moveCursor(QTextCursor.End)
 
-    def on_field_view_clicked(self, x, y):
-        print(f"Clicked at x={x}, y={y}")
-        self.draw_img(pre=(self.mode, self.current_x, self.current_y, x, y))
-        if not confirm(self, f"Вы уверены, что хотите выполнить {self.mode} на координаты x={x}, y={y}?"):
-            return
-        if self.mode != 'G00':
-            self.draw_commands.append((self.mode, self.current_x, self.current_y, x, y))
-        self.current_x = x
-        self.current_y = y
-        self.draw_img()
-        SendGcode(self.portsComboBox.currentText(), [f"{self.mode} X{x} Y{y}",], int(self.baudRateLineEdit.text()))
-
     def on_paint_view_clicked(self, x, y):
-        print(f"Clicked at x={x}, y={y}")
-        # TBD
+        # processing
+        goto_x = x
+        goto_y = y
+        if self.mode == 'hrz':
+            goto_y = self.current_y
+        if self.mode == 'vrt':
+            goto_x = self.current_x
+        if self.mode == 'slp' and self.btn_radio_snapping.isChecked():
+            goto_x, goto_y = nearest_anchor(goto_x, goto_y, self.anchors)
+        print(f"Will go to x={goto_x}, y={goto_y}")
+        # pre-paint and confirm
+        self.draw_img(pre=(self.mode, self.current_x, self.current_y, goto_x, goto_y))  # pre-paint
+        if not confirm(self, f"Вы уверены, что хотите выполнить это действие?"):
+            self.draw_img()
+            return
+        # sending
+        # FIXME: send G-code not shit
+        if not SendGcode(self.portsComboBox.currentText(), [get_gcode(self.mode, self.btn_radio_paint.isChecked(), goto_x, goto_y),], int(self.baudRateLineEdit.text())):
+            alert(self, "Ошибка отправки! Проверьте подключение!")
+            self.draw_img()
+            return
+        # draw if sent succesfully
+        self.g_codes.append(get_gcode(self.mode, self.btn_radio_paint.isChecked(), goto_x, goto_y))
+        if self.btn_radio_paint.isChecked():
+            self.draw_commands.append((self.mode, self.current_x, self.current_y, goto_x, goto_y))
+            # add anchor
+            self.anchors.add((self.current_x, self.current_y))
+            self.anchors.add((goto_x, goto_y))
+        self.current_x = goto_x
+        self.current_y = goto_y
+        self.draw_img()
+
+    def clicked_btn_dump_codes(self):
+        if os.path.exists('g_codes_dump.cnc'):
+            if not confirm(self, "Файл с дампом сущесвует. Вы уверены, что хотите его перезаписать?"):
+                return
+        with open('g_codes_dump.cnc', 'w') as file:
+            file.write('\n'.join(self.g_codes))
+        alert(self, 'G-коды сдаплены в файл "g_codes_dump.cnc" успешно!')
 
     def clicked_btn_calibrate(self):
         if not confirm(self, "Вы уверены, что хотите выполнить калибровку?"):
             return
         self.current_x: int = 0
         self.current_y: int = 0
+        if not SendGcode(self.portsComboBox.currentText(), ["G00 X0 Y0",], int(self.baudRateLineEdit.text())):
+            alert(self, "Ошибка отправки! Проверьте подключение!")
+            return
+        self.g_codes.append('G00 X0 Y0')
         self.draw_img()
-        SendGcode(self.portsComboBox.currentText(), ["G00 X0 Y0",], int(self.baudRateLineEdit.text()))
 
     def clicked_btn_clear_img(self):
         if not confirm(self, "Вы уверены, что хотите очистить картинку?"):
             return
         self.draw_commands = []
+        self.g_codes = []
         print("Image cleared")
         self.draw_img()
 
-    def clicked_btn_g00(self):
-        self.mode = 'G00'
-        self.mode_label.setText(f'Режим: {self.mode}')
+    def clicked_btn_mode_hrz(self):
+        self.mode = 'hrz'
+        self.mode_paint_label.setText(f'Горизонтальное перемещение')
 
-    def clicked_btn_g01(self):
-        self.mode = 'G01'
-        self.mode_label.setText(f'Режим: {self.mode}')
+    def clicked_btn_mode_vrt(self):
+        self.mode = 'vrt'
+        self.mode_paint_label.setText(f'Вертикальное перемещение')
 
-    def clicked_btn_g02(self):
-        pass
+    def clicked_btn_mode_slp(self):
+        self.mode = 'slp'
+        self.mode_paint_label.setText(f'Произвольное перемещение')
 
-    def clicked_btn_g03(self):
-        pass
-
-    def clicked_btn_g90(self):
-        pass
-
-    def clicked_btn_g91(self):
-        pass
 
 if __name__ == "__main__":
     if sys.platform.startswith('win'):
